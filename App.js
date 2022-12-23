319ec7ca92233f3ac1e0f3d8190471bec9080a3a
Vasilis Mpletsos <58275997+VasilisMpletsos@users.noreply.github.com>
2022-12-23 23:29:26 +0200

    Video animation on load screen

--- App.js
+++ App.js
@@ -1,16 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import HomeView from './views/HomeView';
 import ProfileView from './views/ProfileView';
 import ContactsView from './views/Contacts/ContactsView';
 import AddContactsView from './views/Contacts/AddContactsView';
 import CameraView from './views/CameraView';
 import ImpactDetector from './components/ImpactDetector';
+import { Video, AVPlaybackStatus } from 'expo-av';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [showIntro, setShowIntro] = useState(true);
+  const video = React.useRef(null);
+
+  const clearShowIntro = ()=>{
+    setTimeout(()=>{
+      setShowIntro(false)
+    },2700)
+  }
+
+  if(showIntro){
+    return(
+      <View style={styles.container}>
+          <Video
+            ref={video}
+            style={styles.video}
+            source={require('./assets/EasyUpIntro.mp4')}
+            resizeMode="contain"
+            shouldPlay
+            isLooping={false}
+            onReadyForDisplay={clearShowIntro}
+          />
+      </View>
+    )
+  }
+
   return (
     <View style={{flex: 1}}>
       <NavigationContainer>
@@ -26,3 +53,14 @@ export default function App() {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  video: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
